e1d11364fe600337155fbd663c965e720144326c
Sam Fortson <sfortson@uber.com>
2017-11-24 22:35:13 -0500

    Jazz up the site a bit

--- gh-pages-ui/src/App.js
+++ gh-pages-ui/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import vt_logo from './virginia_tech_football.png';
 import './App.css';
 
 class App extends Component {
@@ -8,10 +9,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <a href="https://sfortson.github.io/react-repo" className="App-title">Home</a>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <img src={vt_logo} />
         </p>
       </div>
     );
