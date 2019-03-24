131003ecc9164503fece6dd84edee231281d2686
Samuel Fortson <sfortson@fortsonconsulting.com>
2019-03-24 14:10:32 +0000

    Updates

---
+++ precache-manifest.ac5a7fa2c32c888f814ece76055ccd84.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "bfe908d3bb58ce0d3f37710472e93c85",
+    "url": "/react-repo/static/media/virginia_tech_football.bfe908d3.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-repo/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "dc94e7e1ce4f8dc7e6675f5d1cb34cb7",
+    "url": "/react-repo/static/media/dragons_tooth.dc94e7e1.jpg"
+  },
+  {
+    "revision": "90215516fb023f1939e8",
+    "url": "/react-repo/static/js/runtime~main.95175871.js"
+  },
+  {
+    "revision": "9b4e9855f3e082feb064",
+    "url": "/react-repo/static/js/main.68495ad0.chunk.js"
+  },
+  {
+    "revision": "34dea820e6d77c2ab3f6",
+    "url": "/react-repo/static/js/2.2d8e39a0.chunk.js"
+  },
+  {
+    "revision": "9b4e9855f3e082feb064",
+    "url": "/react-repo/static/css/main.99b19459.chunk.css"
+  },
+  {
+    "revision": "8c61d3909fc3fbc0babbf5e53e56a023",
+    "url": "/react-repo/index.html"
+  }
+];
